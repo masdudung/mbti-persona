964be950a026d2d3f25e09ce3d81b5b31e2811ff
mufiddin <m.mufiddin@gmail.com>
2024-10-03 11:35:11 +0700

    add mikroorm config & migration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
+  dotenv.config();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.APP_PORT;
+  await app.listen(port);
+
+  // Get the address information
+  const address = app.getHttpServer().address();
+
+  // Log the IP address and port
+  const ip = address.address === '::' ? 'localhost' : address.address; // Handle IPv6
+  console.log(`Nest application is running at http://${ip}:${port}`);
 }
 bootstrap();
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,21 +1,17 @@
-// src/mikro-orm.config.ts
-
 import { defineConfig, PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Migrator } from '@mikro-orm/migrations';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 export default defineConfig({
-  // Menambahkan Migrator sebagai ekstensi
   extensions: [Migrator],
-  // Path ke entitas yang sudah terkompilasi dan yang belum
-  entities: ['./dist/entities'],
+  entities: ['./dist/src/entities'],
   entitiesTs: ['./src/entities'],
-  // Menggunakan driver PostgreSQL
   driver: PostgreSqlDriver,
-  // URL koneksi ke database lokal
-  clientUrl: 'postgresql://postgres:lalalala@127.0.0.1:5432/mbti', // Ubah sesuai dengan detail lokal Anda
-  debug: true, // Aktifkan mode debug
+  clientUrl: process.env.DB_CLIENT_URL,
+  debug: process.env.DB_DEBUG === 'true',
   migrations: {
-    tableName: 'mikro_orm_migrations', // Nama tabel untuk menyimpan informasi migrasi
-    path: './migrations', // Jalur ke folder di mana migrasi akan disimpan
+    tableName: 'mikro_orm_migrations',
+    path: './migrations',
   },
 });
